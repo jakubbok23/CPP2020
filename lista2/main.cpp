ff05935d37c47a1e4814a6da1954b81ff7e75cd0
deeckowy <mateuszreis7@gmail.com>
2020-03-09 22:21:02 +0100

    doin_tests

--- lista2/main.cpp
+++ lista2/main.cpp
@@ -7,15 +7,20 @@ int main()
 {
     try
     {
-        punkt x(0.0,1.0);
-        punkt y(1.0,1.2);
-        punkt xx(78.0,1.2);
-        punkt yy(3.0,1.7);
-        prosta xd(x,y);
-        prosta xe(xx,yy);
-        cout<<xd.getter_A()<<" "<<xe.getter_A()<<"  "<<xd.getter_B()<<" "<<xe.getter_B()<<" "<<xd.getter_C()<<" "<<xe.getter_C()<<endl;
-        punkt f(wheree(xe,xd));
-        cout<<f.x<<" "<<f.y<<endl;        
+        //testy wektoruf 
+        wektor w(1.0,1.0);
+        wektor y(w);
+        cout<<dodaj(w,y).dx<<" "<<dodaj(w,y).dy<<endl;
+        
+        //testy punktuf
+        punkt o(69.0,69.0);
+        punkt p(o);
+        punkt x(p,y);
+        cout<<x.x<<" "<<x.y<<endl;
+
+        //testy prostych
+        prosta po(o,x);
+        cout<<po.getter_A()<<" "<<po.getter_B()<<" "<<po.getter_C()<<endl;
     }
     catch(const exception& e)
     {
